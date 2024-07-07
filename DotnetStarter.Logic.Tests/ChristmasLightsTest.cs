071fafc97a013cbc93d858e13e8514b1c42383b8
lokman ERSOY <{ID}+{username}@users.noreply.github.com>
2024-07-07 10:14:20 +0200

    feat add more tests

--- DotnetStarter.Logic.Tests/ChristmasLightsTest.cs
+++ DotnetStarter.Logic.Tests/ChristmasLightsTest.cs
@@ -14,10 +14,16 @@ namespace DotnetStarter.Logic.Tests
         [Theory]
         [InlineData(887, 9, true)]   // Start coordinate
         [InlineData(959, 629, true)] // End coordinate
-        [InlineData(900, 300, true)] // Intermediate point within the range
-        [InlineData(950, 600, true)] // Another intermediate point within the range
+        [InlineData(887, 629, true)] // Intermediate point within the range
+        [InlineData(959, 9, true)] // Another intermediate point within the range
+        [InlineData(900, 250, true)] // Another intermediate point within the range
+        [InlineData(890, 500, true)] // Another intermediate point within the range
+        [InlineData(940, 250, true)] // Another intermediate point within the range
+        [InlineData(910, 15, true)] // Another intermediate point within the range
         [InlineData(886, 8, false)]  // Point outside the range
         [InlineData(960, 630, false)] // Another point outside the range
+        [InlineData(887, 630, false)] // Another point outside the range
+        [InlineData(960, 10, false)] // Another point outside the range
         public void TurnOn(int x, int y, bool expected) {
             int[] coord1 = { 887, 9 };
             int[] coord2 = { 959, 629 };
